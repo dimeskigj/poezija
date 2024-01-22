56f38adc937b03a3daa83e2be3c74fbd940a85ef
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-01-22 20:48:05 +0100

    Implement notice

---
+++ src/app/components/notice/notice.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-notice',
+  standalone: true,
+  imports: [],
+  templateUrl: './notice.component.html',
+  styleUrl: './notice.component.scss'
+})
+export class NoticeComponent {
+  @Output() onClose = new EventEmitter();
+
+  onNoticeClosed(): void {
+    this.onClose.emit();
+  }
+}
